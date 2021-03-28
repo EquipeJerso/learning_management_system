e4e1224177fa53702db038ce0aad8fd93873adcb
Gabriel Fabian <61850709+gabriel-fabian@users.noreply.github.com>
2021-03-27 22:11:16 -0300

    Add courses model
    * Add Course model
    
    * Add courses controller and view
    
    * Update landpage view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'courses/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root 'welcome#index'
+  get 'courses', to: 'courses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
