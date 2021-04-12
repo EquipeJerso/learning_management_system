cdcef4f6c8ad3c4d5d0b28276a056e20fa5f47c8
lucasvns <lucas.hslo@outlook.com>
2021-04-12 20:23:37 -0300

    Implementação Style

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w ( portfolio/logo_transparent2 portfolio/amd portfolio/bd portfolio/logistica portfolio/manutencao portfolio/pea portfolio/gpi portfolio/rita3_07112019210604_21102020164551 sketch-design-mobile )
